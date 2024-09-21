64b27c51ee68bb99995c6d3ba2fbc0bf9b22cbc3
João Elias <12101486@aluno.cotemig.com.br>
2024-09-21 20:30:03 -0300

    Atualização PassageiroBilhete

--- src/business/Passageiro.java
+++ src/business/Passageiro.java
@@ -1,54 +1,66 @@
-    package business;
+package business;
 
-    import java.util.ArrayList;
-    import java.util.Collections;
-    import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-    public class Passageiro {
-        private String rg;
-        private String nome;
-        private String numeroPassaporte;
-        private List<Bilhete> bilhetes;
+public class Passageiro {
+    private String rg;
+    private String nome;
+    private String numeroPassaporte;
+    private List<Bilhete> bilhetes;
 
-        public Passageiro(String nome, String rg, String numeroPassaporte) {
-            this.nome = nome;
-            this.rg = rg;
-            this.numeroPassaporte = numeroPassaporte;
-            this.bilhetes = new ArrayList<>();
-        }
-
-        public boolean adicionarBilhete(Bilhete bilhete) {
-            if (bilhete != null) {
-                return this.bilhetes.add(bilhete);
-            }
-            return false;
-        }
-
-        public boolean removerBilhete(Bilhete bilhete) {
-            return this.bilhetes.remove(bilhete);
-        }
-
-        public List<Bilhete> getBilhetes() {
-            return Collections.unmodifiableList(bilhetes);
-        }
-
-        public String getRg() {
-            return rg;
-        }
-
-        public String getNome() {
-            return nome;
-        }
-
-        public String getNumeroPassaporte() {
-            return numeroPassaporte;
-        }
-
-        public double calcularValorTotalBilhetes() {
-            return bilhetes.stream().mapToDouble(Bilhete::getValorTotal).sum();
-        }
-
-        public double calcularValorTotalSemBagagem() {
-            return bilhetes.stream().mapToDouble(Bilhete::getValorTotalSemBagagem).sum();
-        }
+    public Passageiro(String nome, String rg, String numeroPassaporte) {
+        this.nome = nome;
+        this.rg = rg;
+        this.numeroPassaporte = numeroPassaporte;
+        this.bilhetes = new ArrayList<>();
     }
+
+    public boolean adicionarBilhete(Bilhete bilhete) {
+        if (bilhete != null) {
+            return this.bilhetes.add(bilhete);
+        }
+        return false;
+    }
+
+    public boolean removerBilhete(Bilhete bilhete) {
+        return this.bilhetes.remove(bilhete);
+    }
+
+    public List<Bilhete> getBilhetes() {
+        return Collections.unmodifiableList(bilhetes);
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNumeroPassaporte() {
+        return numeroPassaporte;
+    }
+
+    public void setNumeroPassaporte(String numeroPassaporte) {
+        this.numeroPassaporte = numeroPassaporte;
+    }
+
+    public double calcularValorTotalBilhetes() {
+        return bilhetes.stream().mapToDouble(Bilhete::getValorTotal).sum();
+    }
+
+    public double calcularValorTotalSemBagagem() {
+        return bilhetes.stream().mapToDouble(Bilhete::getValorTotalSemBagagem).sum();
+    }
+}
