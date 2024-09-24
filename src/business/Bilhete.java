dd1274c250c2c40f6515eb7befdaa0a6ca627bbf
pedrodif <pedrodif@icloud.com>
2024-09-24 20:58:24 -0300

    refactor/remodela classes Passageiro e Bilhete

--- src/business/Bilhete.java
+++ src/business/Bilhete.java
@@ -1,22 +1,41 @@
 package business;
 
-import enums.TipoDocumento;
+import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
+
+import enums.TipoDocumento;
 
 public class Bilhete {
 
-    private double valorTotal;
-    private double valorTotalSemBagagem;
-    private TipoDocumento tipoDocumento;
-    private double remuneracaoAgencia;
+    private String codigo;
     private ArrayList<Voo> voos;
+    private Passageiro passageiro;
     private Funcionario funcionario;
+    private TipoDocumento tipoDocumento;
+    private double valorTotalSemBagagem, remuneracaoAgencia, valorTotal;
 
-    public Bilhete(Funcionario funcionario) {
-        this.voos = new ArrayList<>();
+    public Bilhete(Passageiro passageiro, Funcionario funcionario) {
+        this.passageiro = passageiro;
         this.funcionario = funcionario;
+        this.voos = new ArrayList<Voo>();
+        this.codigo = Codigo.gerarCodigoBilhete();
+    }
+
+    public String getCodigo() {
+        return this.codigo;
+    }
+
+    public Passageiro getPassageiro() {
+        return this.passageiro;
+    }
+
+    public Funcionario getFuncionario() {
+        return this.funcionario;
+    }
+
+    public List<Voo> getVoos() {
+        return Collections.unmodifiableList(this.voos);
     }
 
     public void adicionarVoo(Voo voo) {
@@ -27,10 +46,18 @@ public class Bilhete {
         }
     }
 
+    public double getValorTotal() {
+        return this.valorTotal;
+    }
+
     public void calcularValorTotal() {
         this.valorTotal = this.voos.stream()
             .mapToDouble(Voo::getValorPassagem)
             .sum();
+    }
+
+    public double getValorTotalSemBagagem() {
+        return this.valorTotalSemBagagem;
     }
 
     public void calcularValorTotalSemBagagem() {
@@ -39,38 +66,23 @@ public class Bilhete {
             .sum();
     }
 
-    public double calcularRemuneracaoAgencia() {
-        if (this.valorTotalSemBagagem != 0.0) {
-            this.remuneracaoAgencia = this.valorTotalSemBagagem * 0.10;
-        }
+    public double getRemuneracaoAgencia() {
         return this.remuneracaoAgencia;
     }
+
+    public void calcularRemuneracaoAgencia() {
+        this.remuneracaoAgencia = this.valorTotalSemBagagem * 0.10;
+    }
     
-    public List<Voo> getVoos() {
-        return Collections.unmodifiableList(this.voos);
-    }
-
-    public double getValorTotal() {
-        return this.valorTotal;
-    }
-
-    public double getValorTotalSemBagagem() {
-        return this.valorTotalSemBagagem;
-    }
-
     public TipoDocumento getTipoDocumento() {
         return this.tipoDocumento;
     }
 
-    public void setTipoDocumento(TipoDocumento tipoDocumento) {
-        this.tipoDocumento = tipoDocumento;
-    }
-
-    public Funcionario getFuncionario() {
-        return this.funcionario;
-    }
-
-    public double getRemuneracaoAgencia() {
-        return this.remuneracaoAgencia;
+    public void setTipoDocumento(String tipoDocumento) throws Exception {
+        try {
+            this.tipoDocumento = TipoDocumento.valueOf(tipoDocumento);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("Tipo de documento inválido.");
+        }
     }
 }
