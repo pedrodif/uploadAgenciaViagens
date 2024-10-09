12a17a82ffec5d7672decd43be8b8054dd942d6e
Matheus Fernandes <matheus.fer.0063@gmail.com>
2024-10-09 14:39:52 -0300

    Correção de biblioteca

--- src/test/VooTest.java
+++ src/test/VooTest.java
@@ -1,6 +1,7 @@
 package test;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.time.LocalDateTime;
